20fa64a0b0afa3d80df3f6e72780c9c01c54f4ea
dyvosvit <dyvosvit.ukraine@gmail.com>
2017-08-16 18:00:14 +0000

    Update telebt.py

--- telebt.py
+++ telebt.py
@@ -1,7 +1,7 @@
 # it worked for you, you use and like it = donate any amount you wish
-# BTC: 1HRjjHByNL2enV1eRR1RkN698tucecL6FA
+# BTC: 1hHa79zoc4REFWFHwMCRQgW7fYPXJHbpf
 # ETH: 0x4e5e7b86baf1f8d6dfb8a242c85201c47fa86c74
-# ZEC: t1aKAm7qXi6fbGvAhbLioZm3Q8obb4e3BRo
+
 set_debug = False
 
 import imp, sys, pip, time, thread, traceback
